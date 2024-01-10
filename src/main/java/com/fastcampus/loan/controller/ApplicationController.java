6c6b1e9f077711b2dfa58369e2e6f454431ea0aa
jy-hwang <stratojy@gmail.com>
2024-01-11 00:11:26 +0900

    파일 업로드 & 다운로드 기능 추가
    대출 신청시 필요서류를 업로드하고, 업로드한 서류를 확인해볼 수 있는 간단한 기능.
    파일정보를 담은 추가 도메인을 구성해 정보를 따로 관리해볼 수도 있겠다.

--- src/main/java/com/fastcampus/loan/controller/ApplicationController.java
+++ src/main/java/com/fastcampus/loan/controller/ApplicationController.java
@@ -3,9 +3,16 @@ package com.fastcampus.loan.controller;
 import com.fastcampus.loan.dto.ApplicationDTO.AcceptedTermsAndCondition;
 import com.fastcampus.loan.dto.ApplicationDTO.Request;
 import com.fastcampus.loan.dto.ApplicationDTO.Response;
+import com.fastcampus.loan.dto.FileDTO;
 import com.fastcampus.loan.dto.ResponseDTO;
 import com.fastcampus.loan.service.ApplicationService;
+import com.fastcampus.loan.service.FileStorageService;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +20,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +31,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApplicationController extends AbstractController {
 
   private final ApplicationService applicationService;
+
+  private final FileStorageService fileStorageService;
 
   @PostMapping
   public ResponseDTO<Response> create(@RequestBody Request request) {
@@ -51,4 +63,42 @@ public class ApplicationController extends AbstractController {
   public ResponseDTO<Boolean> acceptTerms(@PathVariable Long applicationId, @RequestBody AcceptedTermsAndCondition request) {
     return ok(applicationService.acceptTerms(applicationId, request));
   }
+
+  @PostMapping("/{applicationId}/files")
+  public ResponseDTO<Void> upload(@PathVariable Long applicationId, MultipartFile file) {
+    fileStorageService.save(applicationId, file);
+    return ok();
+  }
+
+  @GetMapping("/{applicationId}/files")
+  public ResponseEntity<Resource> download(@PathVariable Long applicationId, @RequestParam(value = "fileName") String fileName) {
+
+    Resource file = fileStorageService.load(applicationId, fileName);
+
+    return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+
+  }
+
+
+  @GetMapping("/{applicationId}/files/infos")
+  public ResponseDTO<List<FileDTO>> getFileInfos(@PathVariable Long applicationId) {
+    List<FileDTO> fileInfos = fileStorageService.loadAll(applicationId).map(path -> {
+      String fileName = path.getFileName().toString();
+      return FileDTO.builder()
+          .name(fileName)
+          .url(MvcUriComponentsBuilder.fromMethodName(ApplicationController.class, "download", applicationId, fileName).build().toString())
+          .build();
+
+
+    }).collect(Collectors.toList());
+    return ok(fileInfos);
+  }
+
+  @DeleteMapping("/{applicationId}/files")
+  public ResponseDTO<Void> deleteAll(@PathVariable Long applicationId) {
+    fileStorageService.deleteAll(applicationId);
+    return ok();
+  }
+
+
 }
---
+++ src/main/java/com/fastcampus/loan/service/FileStorageService.java
@@ -0,0 +1,20 @@
+package com.fastcampus.loan.service;
+
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FileStorageService {
+
+  void save(Long applicationId, MultipartFile file);
+
+  Resource load(Long applicationId, String fileName);
+
+  void deleteAll(Long applicationId);
+
+  Stream<Path> loadAll(Long applicationId);
+
+
+}
