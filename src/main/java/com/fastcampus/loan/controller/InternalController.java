945dd106c5f482afdd72730ef2fe2c3b255a83e0
jy-hwang <stratojy@gmail.com>
2024-01-16 14:07:23 +0900

    대출 상환 - 상환 기능 CRUD 구현 완료
    대출 상환에 있어서 가장 기본적인 금액 부분에만 집중해서 구현하였음.

--- src/main/java/com/fastcampus/loan/controller/InternalController.java
+++ src/main/java/com/fastcampus/loan/controller/InternalController.java
@@ -3,48 +3,67 @@ package com.fastcampus.loan.controller;
 import com.fastcampus.loan.dto.EntryDTO.Request;
 import com.fastcampus.loan.dto.EntryDTO.Response;
 import com.fastcampus.loan.dto.EntryDTO.UpdateResponse;
+import com.fastcampus.loan.dto.RepaymentDTO;
+import com.fastcampus.loan.dto.RepaymentDTO.ListResponse;
 import com.fastcampus.loan.dto.ResponseDTO;
 import com.fastcampus.loan.service.EntryService;
+import com.fastcampus.loan.service.RepaymentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/internal/applications")
 public class InternalController extends AbstractController {
 
-  // 집행
-  private final EntryService entryService;
+    // 집행
+    private final EntryService entryService;
 
-  // TODO:상환 관련 추가
+    // TODO:상환 관련 추가
+    private final RepaymentService repaymentService;
 
+    @PostMapping("{applicationId}/entries")
+    public ResponseDTO<Response> create(@PathVariable Long applicationId, @RequestBody Request request) {
+        return ok(entryService.create(applicationId, request));
+    }
 
-  @PostMapping("{applicationId}/entries")
-  public ResponseDTO<Response> create(@PathVariable Long applicationId, @RequestBody Request request) {
-    return ok(entryService.create(applicationId, request));
-  }
+    @GetMapping("{applicationId}/entries")
+    public ResponseDTO<Response> get(@PathVariable Long applicationId) {
+        return ok(entryService.get(applicationId));
+    }
 
-  @GetMapping("{applicationId}/entries")
-  public ResponseDTO<Response> get(@PathVariable Long applicationId) {
-    return ok(entryService.get(applicationId));
-  }
+    @PutMapping("/entries/{entryId}")
+    public ResponseDTO<UpdateResponse> update(@PathVariable Long entryId, @RequestBody Request request) {
+        return ok(entryService.update(entryId, request));
+    }
 
-  @PutMapping("/entries/{entryId}")
-  public ResponseDTO<UpdateResponse> update(@PathVariable Long entryId, @RequestBody Request request) {
-    return ok(entryService.update(entryId, request));
-  }
+    @DeleteMapping("/entries/{entryId}")
+    public ResponseDTO<Void> delete(@PathVariable Long entryId) {
+        entryService.delete(entryId);
+        return ok();
+    }
 
+    @PostMapping("{applicationId}/repayments")
+    public ResponseDTO<RepaymentDTO.Response> create(@PathVariable Long applicationId, @RequestBody RepaymentDTO.Request request) {
+        return ok(repaymentService.create(applicationId, request));
+    }
 
-  @DeleteMapping("/entries/{entryId}")
-  public ResponseDTO<Void> delete(@PathVariable Long entryId) {
-    entryService.delete(entryId);
-    return ok();
-  }
+    @GetMapping("{applicationId}/repayments")
+    public ResponseDTO<List<ListResponse>> getPayments(@PathVariable Long applicationId) {
+        return ok(repaymentService.get(applicationId));
+    }
+
+    @PutMapping("repayments/{repaymentId}")
+    public ResponseDTO<RepaymentDTO.UpdateResponse> update(@PathVariable Long repaymentId, @RequestBody RepaymentDTO.Request request) {
+        return ok(repaymentService.update(repaymentId, request));
+    }
+
+    @DeleteMapping("repayments/{repaymentId}")
+    public ResponseDTO<Void> deleeteRepayment(@PathVariable Long repaymentId) {
+        repaymentService.delete(repaymentId);
+        return ok();
+    }
+
 }
